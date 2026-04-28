be5455fc8f7b1baf9084d2e1f52fec16f4030137
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-28 21:56:13 +0300

    add: all necessary models

---
+++ src/config/database.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/MaminirinaEdwino/backmadacare/src/models"
+	"gorm.io/driver/postgres" // Ou mysql
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	// Charge normalement ces infos depuis un fichier .env
+	dsn := "host=localhost user=postgres password=root dbname=seal_db port=5432 sslmode=disable"
+	
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Erreur de connexion à la base de données :", err)
+	}
+
+	fmt.Println("Connexion à la base de données réussie !")
+
+	// Migration automatique des tables (AutoMigrate crée les tables si elles n'existent pas)
+	err = db.AutoMigrate(
+		&models.Etablissement{},
+		&models.Admin{},
+		&models.Personnel{},
+		&models.Patient{},
+		&models.Ambulance{},
+	)
+	
+	if err != nil {
+		log.Fatal("Erreur lors de la migration :", err)
+	}
+
+	DB = db
+}
--- src/models/models.go
+++ src/models/models.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type RequestBody struct {
 	Evidence map[string]int `json:"evidence"`
 	Target   string         `json:"target"`
@@ -8,4 +14,54 @@ type RequestBody struct {
 type ResponseBody struct {
 	Maladie string `json:"maladie"`
 	Urgence string `json:"urgence"`
+}
+
+type Etablissement struct {
+	gorm.Model
+	Nom       string `json:"nom"`
+	Region    string `json:"region"`
+	Contact   string `json:"contact"`
+	Categorie string `json:"categorie"`
+}
+
+type Admin struct {
+	gorm.Model
+	Nom             string        `json:"nom"`
+	Prenom          string        `json:"prenom"`
+	Username        string        `gorm:"unique;not null" json:"username"`
+	Mdp             string        `json:"-"` // "-" pour ne pas exposer le mot de passe en JSON
+	Email           string        `gorm:"unique" json:"email"`
+	EtablissementID uint          `json:"etablissement_id"`
+	Etablissement   Etablissement `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+}
+
+type Personnel struct {
+	gorm.Model
+	Nom             string        `json:"nom"`
+	Prenom          string        `json:"prenom"`
+	Contact         string        `json:"contact"`
+	Poste           string        `json:"poste"`
+	Age             int           `json:"age"`
+	EtablissementID uint          `json:"etablissement_id"`
+	Etablissement   Etablissement `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+}
+
+type Patient struct {
+	gorm.Model
+	Nom             string        `json:"nom"`
+	Prenom          string        `json:"prenom"`
+	Maladies        string        `json:"maladies"` // Stocké en texte ou JSON
+	EtablissementID uint          `json:"etablissement_id"`
+	Etablissement   Etablissement `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+	DateAdmission   time.Time     `json:"date_admission"`
+	DateSortie      *time.Time    `json:"date_sortie"` // Pointeur pour gérer le NULL
+	Status          string        `json:"status"`
+}
+
+type Ambulance struct {
+	gorm.Model
+	Refference  string    `gorm:"unique" json:"refference"`
+	ChauffeurID uint      `json:"chauffeur_id"`
+	Chauffeur   Personnel `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+	Status      string    `json:"status"`
 }
