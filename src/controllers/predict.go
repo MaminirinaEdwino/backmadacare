12d83f24fd580e70e9e0351ab03a7c6e5e303a85
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-29 05:38:28 +0300

    fix: dataset

--- src/controllers/predict.go
+++ src/controllers/predict.go
@@ -27,14 +27,39 @@ func FindPossibleMaladie(prediction map[string]float64) string {
 }
 
 func Predicthandler(w http.ResponseWriter, r *http.Request) {
+	listeSympt := []string{
+		"bokotra manaintaina ao amin'ny ventriny na ankihibe",
+		"tazo tampoka \u003e 39°C",
+		"fandroana be",
+		"mangovitra mafy",
+		"tazo telo andro na efatra andro",
+		"fanaintainana rehefa misitrana",
+		"rà ao amin'ny pivia (hématurie terminale)",
+		"aretin-doha maharitra",
+		"fihazonana ao anatiny ny atidoha (hypertension intracrânienne)",
+		"tsindoka (épilepsie)",
+		"pouls tsy mirindra amin'ny tazo (pouls dissocié)",
+		"tazo maharitra 39-40°C", 
+		"tuphos (fahasahiranana saina sy fadiranovana tanteraka)",
+	}
 	dataUrgence := config.SyncMedicalUrgenceRules("src/config/data/urgence2.json")
 	var req models.RequestBody
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Requête JSON invalide", http.StatusBadRequest)
 		return
 	}
+	var Evidence map[string]int = map[string]int{}
 
-	resultFactor := Network.Query("Maladie", req.Evidence)
+	for _, val := range listeSympt {
+		_, exist := req.Evidence[val]
+		if exist {
+			Evidence[val] = 1
+		}else{
+			Evidence[val] = 0
+		}
+	}
+
+	resultFactor := Network.Query("Maladie", Evidence)
 
 	targetNode, exists := Network.Nodes["Maladie"]
 	if !exists {
